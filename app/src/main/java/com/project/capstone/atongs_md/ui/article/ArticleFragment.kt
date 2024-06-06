dfe3dfb17145f7ea3bf9f6825f59e605023dedf5
naufalmuhamad <2613.muhamad.naufal@gmail.com>
2024-06-07 01:58:22 +0700

    setup to implement api

--- app/src/main/java/com/project/capstone/atongs_md/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/project/capstone/atongs_md/ui/article/ArticleFragment.kt
@@ -4,34 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.project.capstone.atongs_md.databinding.FragmentArticleBinding
+
 
 class ArticleFragment : Fragment() {
 
     private var _binding: FragmentArticleBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val articleViewModel =
-            ViewModelProvider(this)[ArticleViewModel::class.java]
         _binding = FragmentArticleBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        articleViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
